8ffdd24901011ebd5661822b27a628e1348da6dc
Sean Oyler <xrysen@gmail.com>
2021-07-15 23:08:07 -0700

    minor styling updates

--- src/Components/ConfirmModal.tsx
+++ src/Components/ConfirmModal.tsx
@@ -1,27 +1,15 @@
 import "./ConfirmModal.css";
 
 const ConfirmModal = (props: any) => {
-  if (props.success) {
     return (
       <div className = "modal-container">
         <div className = "modal-content">
-          Added to favourites!
+          {props.success ? <span>Added to favourites!</span> : <span>City is already favourited</span> }
           <br />
           <button onClick = {props.onClick}>Okay</button>
         </div>
       </div>
     )
-  }
-
-  return (
-    <div className = "modal-container">
-      <div className = "modal-content">
-        City is already favourited
-        <br />
-        <button onClick = {props.onClick}>Okay</button>
-      </div>
-    </div>
-  )
 }
 
 export default ConfirmModal;
